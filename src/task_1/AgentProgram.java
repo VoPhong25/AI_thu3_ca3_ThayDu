bd1274e63fc8ef45071f90372eef97779a0aefdc
VoPhong25 <voquocphong6678@gmail.com>
2023-10-01 23:08:31 +0700

    lab1

---
+++ src/task_1/AgentProgram.java
@@ -0,0 +1,21 @@
+package task_1;
+
+import task_1.Environment.LocationState;
+
+public class AgentProgram {
+
+	public Action execute(Percept p) {// location, status
+		// TODO
+
+		if (p.getLocationState() == LocationState.DIRTY) {
+			return Environment.SUCK_DIRT;
+		}
+		else if (p.getAgentLocation() == Environment.LOCATION_A) {
+			return Environment.MOVE_RIGHT;
+		} else if (p.getAgentLocation() == Environment.LOCATION_B) {
+			return Environment.MOVE_LEFT;
+		}
+
+		return NoOpAction.NO_OP;
+	}
+}
